b468c8321a7d75a31a21471228f40ddec0e933a7
nokidevz <nokiplays1@gmail.com>
2024-08-14 21:53:38 +0200

    fleur: Link older libs for `camerahalserver`
    * This is required after U QPR2 for `sea`
    * I'll do it in `fleur` as well just in case

--- extract-files.sh
+++ extract-files.sh
@@ -61,6 +61,11 @@ function blob_fixup {
         vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
+        vendor/bin/hw/camerahalserver)
+            "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
+            "${PATCHELF}" --replace-needed "libbinder.so" "libbinder-v32.so" "${2}"
+            ;;
 }
 
 # Initialize the helper
